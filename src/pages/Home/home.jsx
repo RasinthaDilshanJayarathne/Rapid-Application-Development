ba245cce81576ac628a549079c77626245abed57
Radintha Dilshan <rasinthadilshan9@gmail.com>
2022-08-07 00:18:21 +0530

    Update Cards

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -1,7 +1,10 @@
 import { withStyles } from "@mui/styles";
 import { Component } from "react";
 import { styleSheet } from "./styles";
-import { Button, Grid, TextField, Typography,} from "@mui/material";
+import { Button, Grid, TextField, Typography, } from "@mui/material";
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
 
 class Home extends Component {
     constructor(props) {
@@ -14,9 +17,59 @@ class Home extends Component {
             <>
                 <Grid className={classes.container}>
                     <Grid className={classes.navBar}>
-                        <Button style={{width:'150px',backgroundColor:'#D9D9D9',color:'black',marginLeft:'40px',border:'1px solid black'}}>Dashboard</Button>
+                        <Button style={{ width: '150px', backgroundColor: '#D9D9D9', color: 'black', marginLeft: '40px', border: '1px solid black' }}>Dashboard</Button>
+                        <Button style={{ width: '150px', backgroundColor: '#D9D9D9', color: 'black', marginLeft: '450px', border: '1px solid black' }}>Product</Button>
+                        <Button style={{ width: '100px', backgroundColor: '#D9D9D9', color: 'black', marginLeft: '50px', border: '1px solid black' }}>cart</Button>
+                        <Typography variant="h4" gutterBottom component="div" style={{ marginLeft: '350px', marginTop: '10px' }}>
+                            Rasintha
+                        </Typography>
                     </Grid>
-                    <Grid className={classes.bodyContainer}></Grid>
+                    <Grid className={classes.bodyContainer}>
+                        <Grid className={classes.cardBodyContainer}>
+                            <Grid className={classes.card}>
+                                <Card style={{ maxWidth: '500px', maxHeight: '400px'}}>
+                                    <CardContent>
+                                        <Typography gutterBottom variant="h5" component="div">
+                                            Lizard
+                                        </Typography>
+                                        <Typography variant="body2" color="text.secondary">
+                                            Lizards are a widespread group of squamate reptiles, with over 6,000
+                                            species, ranging across all continents except Antarctica
+                                        </Typography>
+                                    </CardContent>
+                                </Card>
+                            </Grid>
+                            <Grid className={classes.card}>
+                            <Card style={{ maxWidth: '500px', maxHeight: '400px'}}>
+                                    <CardContent>
+                                        <Typography gutterBottom variant="h5" component="div">
+                                            Lizard
+                                        </Typography>
+                                        <Typography variant="body2" color="text.secondary">
+                                            Lizards are a widespread group of squamate reptiles, with over 6,000
+                                            species, ranging across all continents except Antarctica
+                                        </Typography>
+                                    </CardContent>
+                                </Card>
+                            </Grid>
+                        </Grid>
+                        <Grid className={classes.cardBodyContainer}>
+                            <Grid className={classes.card}>
+                            <Card style={{ maxWidth: '500px', maxHeight: '400px'}}>
+                                    <CardContent>
+                                        <Typography gutterBottom variant="h5" component="div">
+                                            Lizard
+                                        </Typography>
+                                        <Typography variant="body2" color="text.secondary">
+                                            Lizards are a widespread group of squamate reptiles, with over 6,000
+                                            species, ranging across all continents except Antarctica
+                                        </Typography>
+                                    </CardContent>
+                                </Card>
+                            </Grid>
+                            <Grid className={classes.card}></Grid>
+                        </Grid>
+                    </Grid>
                 </Grid>
             </>
         )
