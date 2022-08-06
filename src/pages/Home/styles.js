cb8783959991c597eb51b31f61af42b5794f0316
Radintha Dilshan <rasinthadilshan9@gmail.com>
2022-08-06 23:34:32 +0530

    Updating

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -2,6 +2,16 @@ export const styleSheet ={
     container:{
         width:'100vw',
         height:'100vh',
+        
+    },
+    navBar:{
+        width:'15vw',
+        height:'100vh',
+        backgroundColor:'black'
+    },
+    bodyContainer:{
+        width:'85vw',
+        height:'100vh',
         backgroundColor:'red'
     }
 }
