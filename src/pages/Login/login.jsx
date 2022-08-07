8aedb0dddb73194f39e4cba3a00c1bbdd94b6d69
Radintha Dilshan <rasinthadilshan9@gmail.com>
2022-08-07 08:11:45 +0530

    Updating Login Page

--- src/pages/Login/login.jsx
+++ src/pages/Login/login.jsx
@@ -3,21 +3,33 @@ import { Component } from "react";
 import { styleSheet } from "./styles";
 import { Button, Grid, TextField, Typography, } from "@mui/material";
 
-class Login extends Component{
-    constructor(props){
+class Login extends Component {
+    constructor(props) {
         super(props)
     }
 
-    render(){
+    render() {
         const { classes } = this.props;
-        return(
+        return (
             <>
-            <Grid className={classes.container}>
-                <Grid className={classes.loginContainer}>
-                    
+                <Grid className={classes.container}>
+                    <Grid className={classes.loginContainer}>
+                        <Grid className={classes.loginHeader}>
+                            <Typography variant="h4" gutterBottom component="div">Login</Typography>
+                        </Grid>
+                        <Grid className={classes.loginFields}>
+                            <TextField id="outlined-basic" label="User Name" variant="outlined" style={{ width: '500px', marginTop: '100px', backgroundColor: '#D9D9D9' }} />
+                            <TextField id="outlined-basic" label="Password" variant="outlined" type="password" style={{ width: '500px', backgroundColor: '#D9D9D9' }} />
+                        </Grid>
+                        <Grid className={classes.loginButton}>
+                            <Button style={{ width: '150px', backgroundColor: '#7C8DEA', color: 'black' }}>Login</Button>
+                        </Grid>
+                        <Grid className={classes.loginClickHere}>
+                            <Typography variant="h6" gutterBottom component="div">Create new user account?Click here</Typography>
+                        </Grid>
+                    </Grid>
                 </Grid>
-            </Grid>
-        </>
+            </>
         )
     }
 }
