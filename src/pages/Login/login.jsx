9c9d14b2a5ba8921641a8ec0f953e3b2a6144c2d
Radintha Dilshan <rasinthadilshan9@gmail.com>
2022-08-07 07:37:20 +0530

    Create Login Page

---
+++ src/pages/Login/login.jsx
@@ -0,0 +1,16 @@
+import { withStyles } from "@mui/styles";
+import { Component } from "react";
+
+class Login extends Component{
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return(
+
+        )
+    }
+}
+
+export default withStyles()(Login)
